615d114430d5be5aa8b7280f3b3de5bf33a396ba
Marshall <marshallmcl@go.byuh.edu>
2025-06-20 09:20:18 -1000

    Final project progress

--- Final-Project/java/script.js
+++ Final-Project/java/script.js
@@ -1,18 +1,3 @@
-const verses = [
-  { category: "Peace", text: "Peace I leave with you..." },
-  { category: "Strength", text: "I can do all things through Christ..." }
-];
-
-let index = 0;
-function rotateVerse() {
-  const verseBox = document.getElementById("scripture-text");
-  verseBox.style.opacity = 0;
-  setTimeout(() => {
-    verseBox.textContent = verses[index].text;
-    verseBox.style.opacity = 1;
-    index = (index + 1) % verses.length;
-  }, 500);
-}
-
-setInterval(rotateVerse, 30000);
-
+document.getElementById('nextScripture').addEventListener('click',function() {
+  document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
